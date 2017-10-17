5dedb373c9e47ea1311338b76b444ecf8b978af1
Gregory Rudnick <grudnick@ku.edu>
2017-10-16 23:03:24 -0500

    now with relative dithers; still needs output file

--- dither_make.py
+++ dither_make.py
@@ -31,36 +31,58 @@ def dither_make(xsize, ysize, npts, mindist, ntol, outfile):
         #this will stay true as long as the preceeding points aren't
         #closer than mindist
         distcheck = True          
-        print(itest,i-itest,distcheck)
         while ((itest >= 0) and (i-itest <= ntol) and distcheck):
             dtest = mt.sqrt( (xtest - x[itest])**2 + (ytest - y[itest])**2)
             
             #if it satisfies the condition then go one further back.
             #If it doesn't then mark as a failure`
             if dtest>=mindist:
-                print("true",i,itest,dtest)
+                #print("true",i,itest,dtest)
                 itest = itest -1
             else:
                 distcheck = False
-                print("false",i,itest,dtest)
+                #print("false",i,itest,dtest)
                 
         #if this point satisfied the proximity condition then keep
         #this point.  Otherwise repeat again
         if distcheck:
             x = np.append(x,xtest)
             y = np.append(y,ytest)
-            i = i + 1
+            i += 1
 
-    #print(x,y)
-    print("hello")
+    #now go through and calculate relative shifts.  
+    xrel = np.array(x[0])
+    yrel = np.array(y[0])
+    i = 1
+    while i < npts:
+        xrel = np.append(xrel, x[i] - x[i - 1])
+        yrel = np.append(yrel, y[i] - y[i - 1])
+        i += 1
+
+    #the last cycle should put the telescope back at the original pointing
+    xrel = np.append(xrel, -x[npts - 1])
+    yrel = np.append(yrel, -y[npts - 1])
+
+    print(x,y)
+    print("")
+    print(xrel,yrel)
+    sumx = np.sum(xrel)
+    sumy = np.sum(yrel)
+    print("")
+    print("these should be zero if the shifts move you back to the origin at the end")
+    print(sumx,sumy)
+    
+            
     plt.clf()
     fig, dithplot = plt.subplots()
     dithplot.plot(x,y)
     dithplot.plot(x,y,'gs')
+    #dithplot.plot(xrel,yrel,'ro',markersize=5)
     dithplot.axis([-50., 50., -50., 50.])
     dithplot.set_xlabel('x')
     dithplot.set_ylabel('y')
     plt.savefig('test.pdf')
     #plt.show()
     
+    #need to write to output file in x,y format
 
